8b0856a4166fadb215b641536c689d5e5455bd14
won <wonsermejor@gmail.com>
2023-05-16 21:37:28 +0900

    calendar

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+  
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      headerToolbar: {
+        left: 'title',
+        right: 'prev,next today'
+      },
+      googleCalendarApikey: 'AIzaSyD24l6QwHnqT0FGyz_X5VDmKhEs8m9a7EY',
+      events: {
+        googleCalendarId: '655f9e0f0e83c29de231a3e7d650fe39bfe082b493f57719c2d1994bf6651e9d@group.calendar.google.com',
+        className: 'gcal-event'
+      }
+    });
+    calendar.render();
+  });
