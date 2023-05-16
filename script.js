ee3c701221b9e69c55df418582a0eab49d6c78be
won <wonsermejor@gmail.com>
2023-05-16 21:51:43 +0900

    d

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
         left: 'title',
         right: 'prev,next today'
       },
-      googleCalendarApiKey: 'AIzaSyD24l6QwHnqT0FGyz_X5VDmKhEs8m9a7EY',
+      googleCalendarApiKey: 'AIzaSyA_a2KxWwdBijNAwSQY2jurgoctyeIW6Ow',
       events: {
-        googleCalendarId: '655f9e0f0e83c29de231a3e7d650fe39bfe082b493f57719c2d1994bf6651e9d@group.calendar.google.com',
-        className: 'gcal-event'
+      googleCalendarId: '655f9e0f0e83c29de231a3e7d650fe39bfe082b493f57719c2d1994bf6651e9d@group.calendar.google.com',
+      className: 'gcal-event'
       }
     });
     calendar.render();
