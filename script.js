4c2f43fe40b84d5bb42428d0d9883566a5e4ee9e
won <wonsermejor@gmail.com>
2023-05-18 17:58:57 +0900

    showjejuinfo

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
-
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    locale: 'ko',
-    initialView: 'dayGridMonth',
-    headerToolbar: {
-      left: 'title',
-      right: 'prev,next today'
-    },
-    footerToolbar: {
-      center: 'prev,next today'
-    },
-    eventSources: {
-    googleCalendarApiKey: 'AIzaSyA_a2KxWwdBijNAwSQY2jurgoctyeIW6Ow',
-    googleCalendarId: '655f9e0f0e83c29de231a3e7d650fe39bfe082b493f57719c2d1994bf6651e9d@group.calendar.google.com',
-    className: 'gcal-event'
-    },
-    eventClick: function(info) {
-      info.jsEvent.stopPropagation();
-      info.jsEvent.preventDefault();  
-      location.href = "https://www.naver.com";
-    }
+    var calendarEl = document.getElementById('calendar');
+  
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      locale: 'ko',
+      initialView: 'dayGridMonth',
+      headerToolbar: {
+        left: 'title',
+        right: 'prev,next today'
+      },
+      footerToolbar: {
+        center: 'prev,next today'
+      },
+      events: {
+        googleCalendarApiKey: 'AIzaSyA_a2KxWwdBijNAwSQY2jurgoctyeIW6Ow',
+        googleCalendarId: '655f9e0f0e83c29de231a3e7d650fe39bfe082b493f57719c2d1994bf6651e9d@group.calendar.google.com',
+        className: 'gcal-event'
+      },
+      eventClick: function(info) {
+        info.jsEvent.stopPropagation();
+        info.jsEvent.preventDefault();  
+        location.href = "https://www.naver.com";
+      }
+    });
+    calendar.render();
   });
-  calendar.render();
-});
